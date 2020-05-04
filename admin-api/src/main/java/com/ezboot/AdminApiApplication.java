25e9b90a78ac0a3b4435090774053611730359c2
davidhua94 <huakang1994@gmail.com>
2020-05-04 18:14:57 +0800

    [update] 测试jenkins提交

--- admin-api/src/main/java/com/ezboot/AdminApiApplication.java
+++ admin-api/src/main/java/com/ezboot/AdminApiApplication.java
@@ -1,12 +1,10 @@
 package com.ezboot;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@Slf4j
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.ezboot")
 @EntityScan(basePackages = "com.ezboot")
