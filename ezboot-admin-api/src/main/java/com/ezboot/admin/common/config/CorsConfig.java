8614ce9b516b21e8febce59cd10565a60d08ed31
david <huakang1994@gmail.com>
2021-01-07 13:02:45 +0800

    [update] 修改项目结构

--- ezboot-admin-api/src/main/java/com/ezboot/admin/common/config/CorsConfig.java
+++ ezboot-admin-api/src/main/java/com/ezboot/admin/common/config/CorsConfig.java
@@ -7,6 +7,9 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * @author david
+ */
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
@@ -21,18 +24,4 @@ public class CorsConfig implements WebMvcConfigurer {
         configSource.registerCorsConfiguration("/**", config);
         return new CorsFilter(configSource);
     }
-
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        //设置允许跨域的路径
-//        registry.addMapping("/**")
-//                //设置允许跨域请求的域名
-//                .allowedOrigins("*")
-//                //是否允许证书 不再默认开启
-//                .allowCredentials(true)
-//                //设置允许的方法
-//                .allowedMethods("*")
-//                //跨域允许时间
-//                .maxAge(3600);
-//    }
-}  
+}
